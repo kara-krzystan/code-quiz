493ca0d541b688fcf2572fcafe9f11f8e5cc7ac5
Kara Krzystan <kara.krzystan@gmail.com>
2021-03-23 18:11:18 -0700

    Basic layout

---
+++ script.js
@@ -0,0 +1,58 @@
+const intro = document.getElementById('intro')
+const startBtn = document.getElementById('start-btn')
+const quiz = document.getElementById('quiz')
+
+const question = document.getElementById('question')
+const answer = document.querySelectorAll('.answer')
+const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+
+
+startBtn.addEventListener("click", startQuiz);
+
+
+function startQuiz() {
+  const current
+}
+
+
+
+const quizContent = [
+  {
+    question: "?",
+    a: "",
+    b: "",
+    c: "",
+    d: "",
+  },
+  {
+    question: "?",
+    a: "",
+    b: "",
+    c: "",
+    d: "",
+  },
+  {
+    question: "?",
+    a: "",
+    b: "",
+    c: "",
+    d: "",
+  },
+  {
+    question: "?",
+    a: "",
+    b: "",
+    c: "",
+    d: "",
+  },
+  {
+    question: "?",
+    a: "",
+    b: "",
+    c: "",
+    d: "",
+  },
+];

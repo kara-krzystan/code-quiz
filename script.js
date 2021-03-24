267321f4115510955e5ed070183f2149ea7ba0b4
Kara Krzystan <kara.krzystan@gmail.com>
2021-03-23 18:47:19 -0700

    Quiz start: Populate questions and answers

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const intro = document.getElementById('intro')
 const startBtn = document.getElementById('start-btn')
 const quiz = document.getElementById('quiz')
 
-const question = document.getElementById('question')
-const answer = document.querySelectorAll('.answer')
+const questionEl = document.getElementById('question')
+const answerEls = document.querySelectorAll('.answer')
 const a_text = document.getElementById('a_text')
 const b_text = document.getElementById('b_text')
 const c_text = document.getElementById('c_text')
@@ -12,20 +12,49 @@ const d_text = document.getElementById('d_text')
 
 startBtn.addEventListener("click", startQuiz);
 
+let currentQuiz = 0
+let score = 0
+
+// startQuiz()
 
 function startQuiz() {
-  const current
+  deselectAnswers()
+  const currentQuizData = quizData[currentQuiz]
+
+    questionEl.innerText = currentQuizData.question
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
+}
+
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false)
+}
+
+function getSelected() {
+    let answer
+
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked) {
+            answer = answerEl.id
+        }
+    })
+
+    return answer
 }
 
 
 
-const quizContent = [
+
+const quizData = [
   {
     question: "?",
-    a: "",
-    b: "",
-    c: "",
-    d: "",
+    a: "aaaa",
+    b: "bbbb",
+    c: "ccc",
+    d: "dddd",
+    correct: "a",
   },
   {
     question: "?",
@@ -33,6 +62,7 @@ const quizContent = [
     b: "",
     c: "",
     d: "",
+    correct: "",
   },
   {
     question: "?",
@@ -40,6 +70,7 @@ const quizContent = [
     b: "",
     c: "",
     d: "",
+    correct: "",
   },
   {
     question: "?",
@@ -47,6 +78,7 @@ const quizContent = [
     b: "",
     c: "",
     d: "",
+    correct: "",
   },
   {
     question: "?",
@@ -54,5 +86,6 @@ const quizContent = [
     b: "",
     c: "",
     d: "",
+    correct: "",
   },
 ];
